a08e0f80783f4b721d6e5cf0a7d08eb2a37a3c2e
chary-vk <vchary43@gmail.com>
2021-12-04 14:19:05 +0530

    changed local credentials to server credentials

--- functions/db.php
+++ functions/db.php
@@ -1,14 +1,14 @@
 <?php
 
-// $host = "bkalrcgh3pkvmk71zsza-mysql.services.clever-cloud.com"; //"localhost";
-// $db = "bkalrcgh3pkvmk71zsza"; //"assignments"; 
-// $user = "u8fhikpnddpazqci"; //"root";
-// $pass = "cyxBkJtvS3pyXbJHO59o";
+$host = "bkalrcgh3pkvmk71zsza-mysql.services.clever-cloud.com"; //"localhost";
+$db = "bkalrcgh3pkvmk71zsza"; //"assignments"; 
+$user = "u8fhikpnddpazqci"; //"root";
+$pass = "cyxBkJtvS3pyXbJHO59o";
 
-$host = "localhost";
-$db = "assignments";
-$user = "root";
-$pass = "";
+// $host = "localhost";
+// $db = "assignments";
+// $user = "root";
+// $pass = "";
 
 
 $conn = mysqli_connect($host, $user, $pass, $db);
